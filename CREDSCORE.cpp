b48398fc7b5c90e00a856ca492b831cf383fbcb8
Sunil y m <59956885+sunilym2277@users.noreply.github.com>
2022-03-19 22:54:03 +0530

    Create CREDSCORE.cpp

---
+++ CREDSCORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x>=750)
+	{
+	    cout<<"YES";
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
+}
