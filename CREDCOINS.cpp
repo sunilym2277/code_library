01a7d3c229c02efa2c197f6d03ef6eb619074ec4
Sunil y m <59956885+sunilym2277@users.noreply.github.com>
2022-03-19 22:53:06 +0530

    Create CREDCOINS.cpp

---
+++ CREDCOINS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    int z=(x*y)/100;
+	    cout<<z<<endl;
+	}
+	return 0;
+}
