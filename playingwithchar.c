c18bd2285a7bcea88d082180837b5396fb022489
Sunil y m <59956885+sunilym2277@users.noreply.github.com>
2022-04-05 20:18:30 +0530

    Create playingwithchar.c

---
+++ playingwithchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch,a[10],b[100];
+scanf("%c%s", &ch,a);
+scanf("\n");
+//scanf("%s",b);
+gets(b);
+printf("%c\n%s\n", ch,a);
+printf("%s\n",b);
+
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
