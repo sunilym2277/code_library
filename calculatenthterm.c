f6c9eba84907ff22018c80dd755e2a593e033d2b
Sunil y m <59956885+sunilym2277@users.noreply.github.com>
2022-04-05 20:14:06 +0530

    Create calculatenthterm.c

---
+++ calculatenthterm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  int p=c;
+  if(c<=n)
+  {
+      p=a+b+c;
+      find_nth_term(n,b,c,p);
+  }
+  return p;
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
